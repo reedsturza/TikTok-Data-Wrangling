65ec10adce1e70995213d9483a90a4fa503edd8f
sturzarmoravian.edu <sturzar@moravian.edu>
2021-12-02 13:57:47 -0500

    update 5

--- toSQL.py
+++ toSQL.py
@@ -8,14 +8,14 @@ import sys
 
 # function creates the connection to mysql and returns the connection variable cnx
 # and the cursor variable used access the database.
-def connectToMySQL():
+def connect_to_my_SQL():
     cnx = mysql.connector.connect(password='project', user='project')
     cursor = cnx.cursor()
     return cursor, cnx
 
 
 # function creates the menagerie database passed as the parameter DB_NAME
-def createDatabase(cursor, DB_NAME):
+def create_database(cursor, DB_NAME):
     # Creates the database at cursor with the given name.
     try:
         cursor.execute(
@@ -25,7 +25,7 @@ def createDatabase(cursor, DB_NAME):
         exit(1)
 
 
-def createTables(cursor):
+def create_tables(cursor):
     # Reads the sql create tables statements and creates the tables
     with open('tables.sql', 'r') as f:
         for line in f:
@@ -42,6 +42,7 @@ def unix_time_to_datetime(unix_time):
 
 
 # https://gist.github.com/slowkow/7a7f61f495e3dbb7e3d767f97bd7304b
+# removes all special character in the strings so that the data is formatted for sql
 def remove_emoji(string):
     emoji_pattern = re.compile("["
                                u"\U0001F600-\U0001F64F"  # emoticons
@@ -67,7 +68,7 @@ def remove_emoji(string):
 
 
 # checks if the musicID is empty, if it is then it returns 0 for the musicID
-def musicIDNotEmpty(musicID):
+def music_id_not_empty(musicID):
     if musicID == '':
         return '0'
     else:
@@ -75,43 +76,39 @@ def musicIDNotEmpty(musicID):
 
 
 # inserts the data into the TikTok table
-def tiktokTable(tiktok, cursor):
-    input = ''
-    input += '"' + tiktok['id'] + '", "' + tiktok['authorMeta']['id'] + '", "' + \
-             musicIDNotEmpty(tiktok['musicMeta']['musicId']) + '", "' + str(
-        unix_time_to_datetime(tiktok['createTime'])[0]) + \
+def tiktok_table(tiktok, cursor):
+    values = '"' + tiktok['id'] + '", "' + tiktok['authorMeta']['id'] + '", "' + \
+             music_id_not_empty(tiktok['musicMeta']['musicId']) + '", "' + str(unix_time_to_datetime(tiktok['createTime'])[0]) + \
              '", "' + str(unix_time_to_datetime(tiktok['createTime'])[1]) + '", "' + \
              remove_emoji(tiktok['text']).replace('\"', "'") + '", "' + str(tiktok['videoMeta']['duration']) + '"'
-    sql = 'INSERT INTO TikTok VALUES (' + input + ');'
+    sql = 'INSERT INTO TikTok VALUES (' + values + ');'
     cursor.execute(sql)
 
 
 # inserts the data into the author table
-def authorTable(tiktok, authors, cursor):
+def author_table(tiktok, authors, cursor):
     if tiktok['authorMeta']['id'] not in authors:
-        input = ''
-        input += '"' + tiktok['authorMeta']['id'] + '", "' + tiktok['authorMeta']['name'] + '", ' + \
+        values = '"' + tiktok['authorMeta']['id'] + '", "' + remove_emoji(tiktok['authorMeta']['name']) + '", ' + \
                  str(tiktok['authorMeta']['verified']) + ', "' + \
                  remove_emoji(tiktok['authorMeta']['signature'].replace('\n', ' ')).replace('\"', "'") + \
                  '", "' + str(tiktok['authorMeta']['fans']) + '"'
-        sql = 'INSERT INTO Author VALUES (' + input + ');'
+        sql = 'INSERT INTO Author VALUES (' + values + ');'
         cursor.execute(sql)
         authors.add(tiktok['authorMeta']['id'])
 
 
 # inserts the data into the music table
-def musicTable(tiktok, sounds, cursor):
+def music_table(tiktok, sounds, cursor):
     # some of the sounds don't have musicAlbum or musicAuthor
     try:
         # uses the sounds set to get rid of duplicates
         if tiktok['musicMeta']['musicId'] not in sounds:
-            input = ''
-            input += '"' + tiktok['musicMeta']['musicId'] + '", "' + \
+            values = '"' + tiktok['musicMeta']['musicId'] + '", "' + \
                      remove_emoji(tiktok['musicMeta']['musicName']).replace('\"', "'") + \
                      '", "' + remove_emoji(tiktok['musicMeta']['musicAuthor']).replace('\"', "'") + \
                      '", ' + str(tiktok['musicMeta']['musicOriginal']) + \
                      ', "' + remove_emoji(tiktok['musicMeta']['musicAlbum']).replace('\"', "'") + '"'
-            sql = 'INSERT INTO Music VALUES (' + input + ');'
+            sql = 'INSERT INTO Music VALUES (' + values + ');'
             cursor.execute(sql)
             sounds.add(tiktok['musicMeta']['musicId'])
     # keyError is for when one of the fields doesn't exist for a tiktok in the json
@@ -120,20 +117,27 @@ def musicTable(tiktok, sounds, cursor):
 
 
 # inserts the data into the tiktok_stats table
-def tiktokStatsTable(tiktok, cursor):
-    input = ''
-    input += '"' + tiktok['id'] + '", "' + str(tiktok['diggCount']) + '", "' + \
+def tiktok_stats_table(tiktok, cursor):
+    values = '"' + tiktok['id'] + '", "' + str(tiktok['diggCount']) + '", "' + \
              str(tiktok['shareCount']) + '", "' + str(tiktok['playCount']) + '", "' + \
              str(tiktok['commentCount']) + '"'
-    sql = 'INSERT INTO Tiktok_Stats VALUES (' + input + ');'
+    sql = 'INSERT INTO Tiktok_Stats VALUES (' + values + ');'
     cursor.execute(sql)
 
 
+# insert the data into the Hash_Tags data
+def hashtag_table(tiktok, cursor):
+    x = 0
+    while x < len(tiktok['hashtags']):
+        values = '"' + tiktok['id'] + '", "' + tiktok['hashtags'][x]['id'] + '", "' + \
+                remove_emoji(tiktok['hashtags'][x]['name']) + '"'
+        sql = 'INSERT INTO Hash_Tags VALUES (' + values + ');'
+        cursor.execute(sql)
+        x += 1
+
+
 # inserts the data into the individual tables
-def insertData(cursor, input_file, ids):
-    # sets the make sure there's no duplicate primary keys in the tables
-    authors = set()
-    sounds = set()
+def insert_data(cursor, input_file, ids, authors, sounds):
     # open the json and extract the data
     with open(input_file) as f:
         data = json.load(f)
@@ -141,20 +145,21 @@ def insertData(cursor, input_file, ids):
             # checks to see if the tiktok is already in the ids set (i.e. the tiktok is already in the csv)
             if tiktok['id'] not in ids:
                 # inserts the data into the TikTok table
-                tiktokTable(tiktok, cursor)
+                tiktok_table(tiktok, cursor)
 
                 # insert the data into the author table
                 # uses the authors set to get rid of duplicate authors
-                authorTable(tiktok, authors, cursor)
+                author_table(tiktok, authors, cursor)
 
                 # insert the data into the music table
-                musicTable(tiktok, sounds, cursor)
+                music_table(tiktok, sounds, cursor)
 
                 # insert data into the tiktok stats data
                 # the primary key is tiktokID so there's already duplication validation
-                tiktokStatsTable(tiktok, cursor)
+                tiktok_stats_table(tiktok, cursor)
 
-
+                # insert the data into the Hash_Tags data
+                hashtag_table(tiktok, cursor)
 
             ids.add(tiktok['id'])
 
@@ -162,19 +167,21 @@ def insertData(cursor, input_file, ids):
 
 def go():
     DB_NAME = 'TikToks'
-    cursor, connection = connectToMySQL()
+    cursor, connection = connect_to_my_SQL()
     cursor.execute('DROP DATABASE IF EXISTS ' + DB_NAME + ';')  # drops the database before it is created
-    createDatabase(cursor, DB_NAME)  # Creates the database
+    create_database(cursor, DB_NAME)  # Creates the database
     cursor.execute("USE {}".format(DB_NAME))
 
     # set to make sure there's no duplicate ids
     ids = set()
-    # # walks through all the files in the TikToks directory and extracts all the data
-    # for root, dirs, files in os.walk('TikToks'):
-    #     for name in files:
-    #         insertData(cursor, os.path.join(root, name), ids)
-    createTables(cursor)
-    insertData(cursor, 'tiktoks10000.json', ids)
+    # sets the make sure there's no duplicate primary keys in the tables
+    authors = set()
+    sounds = set()
+    create_tables(cursor)
+    # walks through all the files in the TikToks directory and extracts all the data
+    for root, dirs, files in os.walk('TikToks'):
+        for name in files:
+            insert_data(cursor, os.path.join(root, name), ids, authors, sounds)
 
     connection.commit()
     cursor.close()
